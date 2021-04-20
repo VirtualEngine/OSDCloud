f8d2128350dbdc717f01535edf075d21b1181a45
nathansperry <nathan.sperry@virtualengine.co.uk>
2021-04-20 10:46:32 +0100

    Create Custom-OSDDeploy.ps1

---
+++ Custom-OSDDeploy.ps1
@@ -0,0 +1,24 @@
+Write-Host  -ForegroundColor Cyan "Starting Virtual Engine's Custom OSDCloud ..."
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+#Make sure I have the latest OSD Content
+Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Cyan "Importing OSD PowerShell Module"
+Import-Module OSD -Force
+
+#Start OSDCloud ZTI the RIGHT way
+Write-Host  -ForegroundColor Green "Start OSDCloud"
+Start-OSDCloud -OSLanguage en-gb -OSBuild 20H2 -OSEdition Enterprise -ZTI
+
+#Restart from WinPE
+Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
