a98c55606a8e18543027b5d359e150ea8cbbc23c
nathansperry <nathan.sperry@virtualengine.co.uk>
2021-08-16 17:29:19 +0100

    Update Custom-OSDDeploy.ps1

--- Custom-OSDDeploy.ps1
+++ Custom-OSDDeploy.ps1
@@ -8,22 +8,22 @@ if ((Get-MyComputerModel) -match 'Virtual') {
 }
 
 #Make sure I have the latest OSD Content
-Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
-Install-Module OSD -Force
+#Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
+#Install-Module OSD -Force
 
-Write-Host -ForegroundColor Cyan "Importing OSD PowerShell Module"
-Import-Module OSD -Force
+#Write-Host -ForegroundColor Cyan "Importing OSD PowerShell Module"
+#Import-Module OSD -Force
 
 #$Global:StartOSDCloud = { ImageFileUrl = "https://prdeuweven.file.core.windows.net/image/VEN-Windows-10-20H2-O365x64-2021-07-21.wim?sv=2019-07-07&sig=5a%2BUd0wRec8nGtFsKakfLjJ0Wd%2FZtGmy5HIzKOQ9HfU%3D&spr=https&se=2021-08-20T17%3A56%3A30Z&srt=co&ss=f&sp=rcwdl"}
-<#
 
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Green "Start OSDCloud"
-Start-OSDCloud -OSLanguage en-gb -OSBuild 20H2 -OSEdition Enterprise -ZTI -SkipAutopilot
+#Start-OSDCloud -OSLanguage en-gb -OSBuild 21H2 -OSEdition Enterprise -ZTI -SkipAutopilot
+Start-OSCloudGUI
 
 #Restart from WinPE
-Read-Host -Prompt "Please remove boot media now and press any key to continue..."
-Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
-Start-Sleep -Seconds 20
-wpeutil reboot
-#>
+#Read-Host -Prompt "Please remove boot media now and press any key to continue..."
+#Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+#Start-Sleep -Seconds 20
+#wpeutil reboot
+
