87019e52784dddc2b1e6261c5ddae195947a3b6a
nathansperry <nathan.sperry@virtualengine.co.uk>
2021-08-16 17:16:54 +0100

    Update Custom-OSDDeploy.ps1

--- Custom-OSDDeploy.ps1
+++ Custom-OSDDeploy.ps1
@@ -14,8 +14,20 @@ Install-Module OSD -Force
 Write-Host  -ForegroundColor Cyan "Importing OSD PowerShell Module"
 Import-Module OSD -Force
 
+## Test we are connected to the internet
+If ((Test-NetConnection google.com -Port 443 -InformationLevel Quiet) -eq $true) {
+
+    Write-Verbose -Message ('Succsessfully connected to the internet')
+    exit
+}
+else {
+
+    Start-WinREWiFi
+}
+
 #$Global:StartOSDCloud = { ImageFileUrl = "https://prdeuweven.file.core.windows.net/image/VEN-Windows-10-20H2-O365x64-2021-07-21.wim?sv=2019-07-07&sig=5a%2BUd0wRec8nGtFsKakfLjJ0Wd%2FZtGmy5HIzKOQ9HfU%3D&spr=https&se=2021-08-20T17%3A56%3A30Z&srt=co&ss=f&sp=rcwdl"}
 <#
+
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Green "Start OSDCloud"
 Start-OSDCloud -OSLanguage en-gb -OSBuild 20H2 -OSEdition Enterprise -ZTI -SkipAutopilot
