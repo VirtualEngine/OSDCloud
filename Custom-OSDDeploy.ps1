b213b5fb957ff2d79f664aecaf6cc036020d4f3c
nathansperry <nathan.sperry@virtualengine.co.uk>
2021-08-16 17:39:14 +0100

    Update Custom-OSDDeploy.ps1

--- Custom-OSDDeploy.ps1
+++ Custom-OSDDeploy.ps1
@@ -19,7 +19,7 @@ Import-Module OSD -Force
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Green "Start OSDCloud"
 #Start-OSDCloud -OSLanguage en-gb -OSBuild 21H2 -OSEdition Enterprise -ZTI -SkipAutopilot
-#Start-OSCloudGUI
+Start-OSCloudGUI
 
 #Restart from WinPE
 #Read-Host -Prompt "Please remove boot media now and press any key to continue..."
