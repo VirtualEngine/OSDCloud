84bc239c156e9eb2294d043a80207d339147519b
nathansperry <nathan.sperry@virtualengine.co.uk>
2021-08-17 14:26:44 +0100

    Update Custom-OSDDeploy.ps1

--- Custom-OSDDeploy.ps1
+++ Custom-OSDDeploy.ps1
@@ -19,7 +19,7 @@ Import-Module OSD -Force
 #$Global:StartOSDCloud = { ImageFileUrl = "https://prdeuweven.file.core.windows.net/image/VEN-Windows-10-20H2-O365x64-2021-07-21.wim?sv=2019-07-07&sig=5a%2BUd0wRec8nGtFsKakfLjJ0Wd%2FZtGmy5HIzKOQ9HfU%3D&spr=https&se=2021-08-20T17%3A56%3A30Z&srt=co&ss=f&sp=rcwdl"}
 
 #Start OSDCloud ZTI the RIGHT way
-Write-Host  -ForegroundColor Green "Start OSDCloud"
+#Write-Host  -ForegroundColor Green "Start OSDCloud"
 #Start-OSDCloud -OSLanguage en-gb -OSBuild 21H2 -OSEdition Enterprise -ZTI -SkipAutopilot
 #Start-OSCloudGUI
 
